8cd19f4c0dbe731c8400ca9f05f6748efb909405
Jesse <jesseorhs@gmail.com>
2023-05-11 13:27:33 -0700

    Create instance method on UserSchema, generate JWT for register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,16 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError} = require('../errors')
-const bycrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-    const {name, email, password} = req.body
-    const salt = await bycrypt.genSalt(10);
-    const hashedPassword = await bycrypt.hash(password,salt)
-
-    const tempUser = {name, email, password: hashedPassword}
-
-
-    const user = await User.create({...tempUser})
-    res.status(StatusCodes.CREATED).json({ user })
+    const user = await User.create({...req.body})
+    const token = user.createJWT()
+    res
+        .status(StatusCodes.CREATED)
+        .json({user:{name:user.name}, token })
 }
+
+
 const login = async (req, res) => {
     res.send('login user')
 }
