0410114fc913a8e56a1d72fbfc2cd220a66b9161
Jesse <jesseorhs@gmail.com>
2023-05-11 13:03:42 -0700

    Add password hashing to register controller with bycryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,17 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+const bycrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-    const user = await User.create({...req.body})
+    const {name, email, password} = req.body
+    const salt = await bycrypt.genSalt(10);
+    const hashedPassword = await bycrypt.hash(password,salt)
+
+    const tempUser = {name, email, password: hashedPassword}
+
+
+    const user = await User.create({...tempUser})
     res.status(StatusCodes.CREATED).json({ user })
 }
 const login = async (req, res) => {
