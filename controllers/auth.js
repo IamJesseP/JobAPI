0a7f6f51fd5c09e045ef384dd4f28810a171aa1b
Jesse <jesseorhs@gmail.com>
2023-05-11 11:52:06 -0700

    Setup structure for CRUD controllers, as well as reg/login

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+const register = async (req, res) => {
+    res.send('register user')
+}
+const login = asytnc (req, res) => {
+    res.send('login user')
+}
+
+module.exports = {
+    register,
+    login,
+}
