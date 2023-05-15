2cc340bfc3a14946914a0566a1e71f1a5cc6ed3c
Jesse <jesseorhs@gmail.com>
2023-05-15 16:51:55 -0700

    Add security packages

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+//security
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 //connectDB
 const connectDB = require('./db/connect')
@@ -16,7 +21,15 @@ const notFoundMiddleware = require('../content/middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
+// security
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
